b19fdbdc867d0636817d90512693576fc9b5aef5
Dragos <dragosfs@gmail.com>
2025-12-16 21:34:10 +0200

    Initialize minigrep project with basic structure and functionality

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::from_args(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    run(config);
+
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn from_args(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("{contents}");
+
+    Ok(())
+}
